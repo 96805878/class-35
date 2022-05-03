55d26ae00a31a907be1ea993768f53fd16ae8460
96805878 <92256586+96805878@users.noreply.github.com>
2022-05-03 19:34:14 +0800

    Add files via upload

---
+++ Objects/form.js
@@ -0,0 +1,22 @@
+class Form{
+    constructor(){
+        
+    }
+    display(){}
+        
+
+}
+
+/*
+p5.DOM - Document Object Model
+
+HTML:
+    1. Head - info about the page
+    2. Body - visible content on the page
+
+BODY:
+    1. Input box
+    2. Play button
+    3. Title
+    4. Greeting message
+*/
---
+++ Objects/game.js
@@ -0,0 +1,23 @@
+class Game{
+    constructor(){
+
+    }
+
+    start(){
+        form=new Form()
+        form.display()
+    }
+    
+    play(){
+
+    }
+
+    end(){
+        
+    }
+
+
+}
+
+
+
